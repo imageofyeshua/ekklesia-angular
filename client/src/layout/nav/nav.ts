76a169235da5dfebe64b19cb1cb865dd2ad3bfd0
Daniel Park <intheimageofyeshua@gmail.com>
2025-11-27 17:43:44 +0900

    Adding Router to Nav

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,20 +10,22 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './nav.css',
 })
 export class Nav {
-  protected accountService = inject(AccountService)
-  protected creds: any = {}
+  protected accountService = inject(AccountService);
+  private router = inject(Router);
+  protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => {
-        console.log(result);
+      next: (result) => {
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message)
-    })
+      error: (error) => alert(error.message),
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
