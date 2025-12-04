0decf7c3b2d41d13412ca2d35b7dd8655ddb7c88
Daniel Park <intheimageofyeshua@gmail.com>
2025-12-04 09:46:16 +0900

    Create Member and Photo Type

---
+++ client/src/types/member.ts
@@ -0,0 +1,19 @@
+export type Member = {
+  id: string;
+  dateOfBirth: string;
+  imageUrl?: string;
+  displayName: string;
+  created: string;
+  lastActive: string;
+  sex?: string;
+  description?: string;
+  city?: string;
+  country?: string;
+};
+
+export type Photo = {
+  id: number;
+  url: string;
+  publicId?: string;
+  memberId: string;
+};
